edad29bb7525af395eba8c45ee1f458d6f08e3b4
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-11-24 23:48:01 +0500

    Update qr_gen.py

--- QR Code Generator/qr_gen.py
+++ QR Code Generator/qr_gen.py
@@ -11,3 +11,5 @@ qr_code.add_data(input_data)
 qr_code.make(fit = True)
 
 # Converting QR Code Into Image
+qr_image = qr_code.make_image(fill = "black", back_colour = "white")
+qr_image.save("qrcode001.png")
