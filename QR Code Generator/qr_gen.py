58854f2f5b9e4b83b18a9d0ec13b8a9293621444
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-11-24 23:45:12 +0500

    Update qr_gen.py

--- QR Code Generator/qr_gen.py
+++ QR Code Generator/qr_gen.py
@@ -1,2 +1,13 @@
 import qrcode
-qrcode.QRCode()
+
+# Input Data 
+input_data = " "
+
+# Instance Of QR Code 
+qr_code = qrcode.QRCode(version = 1, box_size = 10, border = 5)
+
+# Adding Input Data 
+qr_code.add_data(input_data)
+qr_code.make(fit = True)
+
+# Converting QR Code Into Image
