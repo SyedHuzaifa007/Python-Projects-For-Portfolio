28e402283d9ec601a3a094f153ec7532a91064f6
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-12-05 23:44:20 +0500

    Creating Currency Converter Class

--- Currency Converter/converter.py
+++ Currency Converter/converter.py
@@ -4,3 +4,10 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
+# Creating Curerency Converter Class
+class Currency_converer():
+    def __init__(self, url):
+        self.data = requests.get(url).json()
+        self.currencies = self.data['rates']
+
+        
