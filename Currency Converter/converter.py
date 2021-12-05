6889271c6ad3dbdba96b280c7d29156ae4bbfdd5
Syed Huzaifa <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-12-05 23:48:23 +0500

    Creating Converter Method

--- Currency Converter/converter.py
+++ Currency Converter/converter.py
@@ -4,10 +4,17 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-# Creating Curerency Converter Class
+# Creating Currency Converter Class
 class Currency_converer():
     def __init__(self, url):
         self.data = requests.get(url).json()
         self.currencies = self.data['rates']
 
+# Creating Converter Method
+def convert(self, from_currency, to_currency, amount):
+    initial_amount = amount
+    if from_currency != 'USD':
+        amount = amount / self.currencies[from_currency]
+        amount = round(amount * self.currencies[to_currency], 4)
+        return amount
         
