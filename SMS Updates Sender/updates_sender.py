ba035164b2b14438a937d97cbc55e2b094b642a8
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-12-23 23:33:24 +0500

    Update updates_sender.py

--- SMS Updates Sender/updates_sender.py
+++ SMS Updates Sender/updates_sender.py
@@ -1,7 +1,18 @@
 # Importing Required Moduels
+from contextlib import closing
 import time
-from time import sleep
+from time import clock_gettime_ns, sleep
 from sinchsms import SinchSMS
 
 # Defining Fucntion For Sending SMS
+def send_sms():
+    number = ''
+    app_key = ''
+    app_secret = ''
 
+    message = ''
+
+    client = SinchSMS(app_key, app_secret)
+    print("Sending '%s' to '%s'" % (message, number))
+
+    
