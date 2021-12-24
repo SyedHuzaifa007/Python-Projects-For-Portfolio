8f507db37edc9aab2369d52b2557a48e1e0366be
SyedHuzaifa007 <huzaifanau123@gmail.com>
2021-12-24 18:03:37 +0500

    Provided Access Token

--- Whatsapp Chat History Exporter/exporter.py
+++ Whatsapp Chat History Exporter/exporter.py
@@ -1,3 +1,12 @@
+from typing import Text
 import urllib.request
 import pandas as pd
-f
+from pushbullet import PushBullet
+
+Access_token = 'Your Access Token Here'
+pb = PushBullet(Access_token)
+all_pushes = pb.get_pushes()
+latest_one = all_pushes[0]
+url = latest_one['file.url']
+Text_file = "All_Chats.txt"
+urllib.request.urlretrieve(url, Text_file)
